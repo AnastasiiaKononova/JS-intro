ef275bc7918b71cb5a3d12803845213c46c15ad6
Anastasiia <anastasiiakononova0711@gmail.com>
2024-08-06 15:45:28 +0300

    Add consult practice method arrays

--- assets/js/MyArray.js
+++ assets/js/MyArray.js
@@ -1,43 +1,136 @@
-function MyArray(...argums) {
-// new створює пустий обєкт {} і кладе його в this
-this.length = 0;
-for ( let i = 0; i < argums.length; i++) {
-    this.push(argum[i])
-}
+function MyArray(...argums){
+    // new створює пустий {} і кладе його в this
+    this.length = 0;
+    this.push(...argums);
 
 }
+
 
 function MyArrayProto() {
-    this.push = function(v) {
-        this[this.length] = v;
+    this.push = function (...v) {
+       for(let i = 0; i < v.length; i++){
+        this[this.length] = v[i];
         this.length++;
+       }
         return this.length
     }
-    
-    this.pop = function() {
-        // взяти останній елемент, видалитий його
-        let temporary = this[this.length - 1];
-        delete this[this.length - 1];
-        //delete obj.property
-        //зменшити довжину на один
-        this.length--;
-        return temporary;
     }
-    
-    this.forEach = function(fn) {
+
+
+    this.forEach = function(fn, thisArg) {
         for (let i = 0; i < this.length; i++) {
-            fn(this[i]);
+          fn.call(thisArg, this[i], i, this)         
         }
     }
-    
-}
-
-MyArray.prototype = MyArrayProto();
 
 
+    this.concat = function(array2) {
+        /*  Викликається на екземплярі масиву, отримує другий масив в аргументи і повертає новий = масив1 + масив2 */
+        const result = new MyArray();
+        this.forEach(el => result.push(el));
+        array2.forEach(el => result.push(el));
 
-const arr = new MyArray();
+        return result;
+    }
 
+    this.at = function(index) {
+        if(index < 0) {
+            return this[this.length + index];
+        }
+        return this[index]
+    }
+
+    this.fill = function(value, start = 0, end = this.length) {
+        if(start < 0) {
+            start = this.length + start;
+        }
+        if(end < 0) {
+            end = this.length + end
+        }
+        for (let i = start; i < end; i++) {
+            this[i] = value;
+        }
+        return this
+    }
+
+    this.filter = function(filteringFunction, thisArg) {
+        const result = new MyArray();
+        for (let i = 0; i < this.length; i++) {         
+            if(filteringFunction.call(thisArg, this[i], i, this)){
+                result.push(this[i])
+            }
+        }
+        return result
+    }
+
+    this.find = function(checkingFunction, thisArg) {
+        for (let i = 0; i < this.length; i++) {
+            if(checkingFunction.call(thisArg, this[i], i, this)){
+                return this[i]
+            }
+        }
+    }
+
+
+    this.join = function(separator = ','){
+        let str = '';
+        for (let i = 0; i < this.length - 1; i++) {
+           str += this[i] + separator;
+        }
+        str += this[this.length-1];
+        return str;
+    }
+
+
+
+    this.map = function(fn, thisArg) {
+        const res = new MyArray();
+        for (let i = 0; i < this.length; i++) {
+           res.push(fn.call(thisArg, this[i], i, this));            
+        }
+    }
+
+
+    this.slice = function(start = 0, end = this.length) {
+        if (start < 0) {
+            start = this.length + start;
+        }
+        if( start > this.length) {
+            return new MyArray();
+        }
+        if(end < 0) {
+            end = this.length + end;
+        }
+        const res = new MyArray();
+        for (let i = start; i < end; i++) {
+           res.push(this[i]) 
+        }
+        return res;
+    }
+
+    this.sort = function(compareFunction) {
+        for (let j=0; j < this.length; j++) {
+            for (let i = 0; i < this.length; i++) {
+                if (compareFunction(this[i], this[i+1]) > 0){
+                    // поміняти місцями елементи
+                    let temporary = this[i];
+                    this[i] = this[i+1];
+                    this[i+1] = temporary;
+
+                } else {
+                    // залишаємо елементи на місцях 
+
+                }
+
+            }
+        }
+
+        return this
+    }
+
+
+
+MyArray.prototype = new MyArrayProto();
 /*
 ДЗ: 
 доповнити колекцію MyArray реалізацією методів нативного масива:
@@ -51,3 +144,6 @@ const arr = new MyArray();
 - slice
 - sort
 */
+
+
+
